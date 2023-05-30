249411246cf9328ac82fc3d7cbe410ed608b80eb
Nilay Sharma <nilaysharma642@gmail.com>
2023-05-30 23:49:40 +0530

    update voting contract

--- Voting.sol
+++ Voting.sol
@@ -2,5 +2,27 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Ballot {
-  
+  // This declares a new complex type which will be used for variables later, it represents a single voter.
+  struct Voter {
+    uint weight;
+    bool voted;
+    address delegate;
+    uint vote;
+  }
+
+  struct Proposal {
+    bytes32 name;
+    uint voteCount;
+  }
+
+  address public chairperson;
+
+  // This declates a state variable that stores a `Voter struct for each possible address.
+  mapping(address => Voter) public voters;
+
+  // A dynamically-sized array of `Proposal` structs.
+  Proposal[] public proposals;
+
+  // Create a new ballot to choose one of `proposalNames`
+
 }
